864f89e9fe7fbc6bf41e74c444456113d744d8c2
Parsh-Dev <parsh.cosmos11@gmail.com>
2018-03-24 00:27:08 +0530

    En/De-Cryptor version 1.0

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -7,38 +7,135 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   var key;
   var message;
+  var cipher;
+  var ciphertext = "";
+  var decipher;
+  var decipherText = "";
+  var blockCipher;
+  TextEditingController messageController;
+  TextEditingController keyController;
 
-  String encoder(var key, var message){ 
-  this.key = key;
-  var blockCipher = new BlockCipher(new DESEngine(), key);
-  this.message = message;
-  var ciphertext = blockCipher.encodeB64(message);
-  var decoded = blockCipher.decodeB64(ciphertext);
-
-  print("key: $key");
-  print("message: $message");
-  print("ciphertext (base64): $ciphertext");
-  print("decoded ciphertext: $decoded");
-  return ciphertext;
+  encoder() {
+    setState(() {
+      this.key = keyController.text;
+      this.blockCipher = new BlockCipher(new DESEngine(), key);
+      this.message = messageController.text;
+      this.cipher = blockCipher.encodeB64(message);
+      this.ciphertext = 'CipherText: "$cipher"';
+    });
   }
-  
+
+  decoder() {
+    setState(() {
+      this.decipher = blockCipher.decodeB64(this.cipher);
+      this.decipherText = 'DecipherText: "$decipher"';
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    messageController = new TextEditingController();
+    keyController = new TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    messageController.dispose();
+    keyController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      backgroundColor: Colors.blue,
-      body: new Container(
-        child: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Text(encoder("Hey there", "Let's rock and roll baby"), style: new TextStyle(color: Colors.white, fontSize: 20.0),),
-            ],
-          ),
+        appBar: new AppBar(
+          title: new Text("#En/De-Cryptor"),
+          backgroundColor: Colors.blue,
         ),
-      ),
-    );
+        backgroundColor: Colors.white,
+        body: new ListView(
+          children: <Widget>[
+            new Container(
+              padding: new EdgeInsets.only(top: 50.0),
+              child: new Center(
+                child: new Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    new Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: new TextField(
+                        decoration: new InputDecoration(
+                          hintText: 'Enter the message',
+                          hintStyle: new TextStyle(fontSize: 21.0),
+                        ),
+                        controller: messageController,
+                      ),
+                    ),
+                    new Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: new TextField(
+                        decoration: new InputDecoration(
+                          hintText: 'Enter the key',
+                          hintStyle: new TextStyle(fontSize: 21.0),
+                        ),
+                        controller: keyController,
+                      ),
+                    ),
+                    new Padding(
+                      padding:
+                          const EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 30.0),
+                      child: new RaisedButton(
+                        color: Colors.blue,
+                        child: new Text(
+                          "Encrypt",
+                          style: new TextStyle(
+                              fontSize: 20.0, color: Colors.white),
+                        ),
+                        onPressed: encoder,
+                      ),
+                    ),
+                    new Padding(
+                      padding:
+                          const EdgeInsets.all(10.0),
+                      child: new Text(
+                        ciphertext,
+                        textAlign: TextAlign.center,
+                        style: new TextStyle(fontSize: 19.0),
+                      ),
+                    ),
+                    new Opacity(
+                      opacity: this.cipher == null ? 0.0 : 1.0,
+                      child: new Padding(
+                        padding:
+                            const EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 30.0),
+                        child: new RaisedButton(
+                          color: Colors.blue,
+                          child: new Text(
+                            "Decrypt",
+                            style: new TextStyle(
+                                fontSize: 20.0, color: Colors.white),
+                          ),
+                          onPressed: decoder,
+                        ),
+                      ),
+                    ),
+                    new Padding(
+                      padding:
+                          const EdgeInsets.all(10.0),
+                      child: new Text(
+                        decipherText,
+                        textAlign: TextAlign.center,
+                        style: new TextStyle(fontSize: 19.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
