79e4b5bfe6f74b16579e45d26a7cb435406a4f86
Parsh-Dev <parsh.cosmos11@gmail.com>
2018-03-24 12:32:04 +0530

    v2.0

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -14,6 +14,8 @@ class _HomePageState extends State<HomePage> {
   var decipher;
   var decipherText = "";
   var blockCipher;
+  bool messageFlag = false;
+  bool keyFlag = false;  
   TextEditingController messageController;
   TextEditingController keyController;
 
@@ -25,6 +27,11 @@ class _HomePageState extends State<HomePage> {
       this.cipher = blockCipher.encodeB64(message);
       this.ciphertext = 'CipherText: "$cipher"';
     });
+
+    Scaffold.of(context).showSnackBar(new SnackBar(
+          content: new Text("Encrypted!"),
+          duration: new Duration(seconds: 1),
+        ));
   }
 
   decoder() {
@@ -32,13 +39,20 @@ class _HomePageState extends State<HomePage> {
       this.decipher = blockCipher.decodeB64(this.cipher);
       this.decipherText = 'DecipherText: "$decipher"';
     });
+
+    Scaffold.of(context).showSnackBar(new SnackBar(
+          content: new Text("Decrypted!"),
+          duration: new Duration(seconds: 1),
+        ));
   }
+
 
   @override
   void initState() {
     super.initState();
     messageController = new TextEditingController();
     keyController = new TextEditingController();
+    print("text" + messageController.text);
   }
 
   @override
@@ -50,92 +64,88 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-        appBar: new AppBar(
-          title: new Text("#En/De-Cryptor"),
-          backgroundColor: Colors.blue,
-        ),
-        backgroundColor: Colors.white,
-        body: new ListView(
-          children: <Widget>[
-            new Container(
-              padding: new EdgeInsets.only(top: 50.0),
-              child: new Center(
-                child: new Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    new Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: new TextField(
-                        decoration: new InputDecoration(
-                          hintText: 'Enter the message',
-                          hintStyle: new TextStyle(fontSize: 21.0),
-                        ),
-                        controller: messageController,
-                      ),
-                    ),
-                    new Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: new TextField(
-                        decoration: new InputDecoration(
-                          hintText: 'Enter the key',
-                          hintStyle: new TextStyle(fontSize: 21.0),
-                        ),
-                        controller: keyController,
-                      ),
-                    ),
-                    new Padding(
-                      padding:
-                          const EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 30.0),
-                      child: new RaisedButton(
-                        color: Colors.blue,
-                        child: new Text(
-                          "Encrypt",
-                          style: new TextStyle(
-                              fontSize: 20.0, color: Colors.white),
-                        ),
-                        onPressed: encoder,
-                      ),
-                    ),
-                    new Padding(
-                      padding:
-                          const EdgeInsets.all(10.0),
-                      child: new Text(
-                        ciphertext,
-                        textAlign: TextAlign.center,
-                        style: new TextStyle(fontSize: 19.0),
-                      ),
-                    ),
-                    new Opacity(
-                      opacity: this.cipher == null ? 0.0 : 1.0,
-                      child: new Padding(
-                        padding:
-                            const EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 30.0),
-                        child: new RaisedButton(
-                          color: Colors.blue,
-                          child: new Text(
-                            "Decrypt",
-                            style: new TextStyle(
-                                fontSize: 20.0, color: Colors.white),
-                          ),
-                          onPressed: decoder,
-                        ),
-                      ),
-                    ),
-                    new Padding(
-                      padding:
-                          const EdgeInsets.all(10.0),
-                      child: new Text(
-                        decipherText,
-                        textAlign: TextAlign.center,
-                        style: new TextStyle(fontSize: 19.0),
-                      ),
-                    ),
-                  ],
+    return new ListView(
+      children: <Widget>[
+        new Container(
+          padding: new EdgeInsets.only(top: 35.0),
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: new TextField(
+                  decoration: new InputDecoration(
+                    hintText: 'Enter the message',
+                    hintStyle: new TextStyle(fontSize: 21.0),
+                  ),
+                  controller: messageController,
+                  onChanged: (String str){
+                    setState((){
+                    this.messageFlag = str.trim() == ""? false: true;
+                    });
+                  },  
                 ),
               ),
-            ),
-          ],
-        ));
+              new Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: new TextField(
+                  decoration: new InputDecoration(
+                    hintText: 'Enter the key',
+                    hintStyle: new TextStyle(fontSize: 21.0),
+                  ),
+                  controller: keyController,
+                  onChanged:(String str){
+                    setState((){
+                    this.keyFlag = str.trim() == ""? false: true;
+                    });
+                  }, 
+                ),
+              ),
+              new Padding(
+                padding: const EdgeInsets.fromLTRB(10.0, 50.0, 10.0, 30.0),
+                child: new RaisedButton(
+                  color: Colors.blue,
+                  child: new Text(
+                    "Encrypt",
+                    style: new TextStyle(fontSize: 21.0, color: Colors.white),
+                  ),
+                  onPressed: this.messageFlag == false || this.keyFlag == false ? null : encoder,
+                ),
+              ),
+              new Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: new Text(
+                  ciphertext,
+                  textAlign: TextAlign.center,
+                  style: new TextStyle(fontSize: 19.0),
+                ),
+              ),
+              new Opacity(
+                opacity: this.cipher == null ? 0.0 : 1.0,
+                child: new Padding(
+                  padding: const EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 30.0),
+                  child: new RaisedButton(
+                    color: Colors.blue,
+                    child: new Text(
+                      "Decrypt",
+                      style: new TextStyle(fontSize: 21.0, color: Colors.white),
+                    ),
+                    onPressed: decoder,
+                  ),
+                ),
+              ),
+              new Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: new Text(
+                  decipherText,
+                  textAlign: TextAlign.center,
+                  style: new TextStyle(fontSize: 19.0),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,14 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new HomePage()
-    );
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("#En/De-Cryptor"),
+          backgroundColor: Colors.blue,
+        ),
+        backgroundColor: Colors.white,
+        body: new HomePage()
+    )
+  );
   }
 }
